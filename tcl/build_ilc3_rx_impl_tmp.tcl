718e133af889370bbb372ac51457ebbd1e4ababd
knagjet <jungjet68@gmail.com>
2026-04-28 11:13:36 +0900

    Record stable ILC3 one-hour link baseline

--- tcl/build_ilc3_rx_impl_tmp.tcl
+++ tcl/build_ilc3_rx_impl_tmp.tcl
@@ -1,13 +1,7 @@
-open_project {C:/zybo_z7_20_prebringup/build/vivado_rx/ilc3_rx_board.xpr}
+open_project C:/zybo_z7_20_prebringup/build/vivado_rx/ilc3_rx_board.xpr
 reset_run synth_1
+launch_runs synth_1 -jobs 4
+wait_on_run synth_1
 reset_run impl_1
 launch_runs impl_1 -to_step write_bitstream -jobs 4
 wait_on_run impl_1
-
-set bitfile [glob -nocomplain {C:/zybo_z7_20_prebringup/build/vivado_rx/ilc3_rx_board.runs/impl_1/*.bit}]
-if {[llength $bitfile] == 0} {
-    puts stderr "RX bitstream was not generated."
-    exit 1
-}
-
-puts "RX bitstream: [lindex $bitfile 0]"
