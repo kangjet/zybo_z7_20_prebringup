b58249c162e522e25f7d13aa718b19aef13c7307
knagjet <jungjet68@gmail.com>
2026-05-28 13:13:14 +0900

    Force synth rebuild for histogram TAG RX

--- tcl/run_impl_ilc3_histogram_tag_packet256_rx.tcl
+++ tcl/run_impl_ilc3_histogram_tag_packet256_rx.tcl
@@ -1,4 +1,5 @@
 open_project C:/zybo_z7_20_prebringup/build/vivado_ilc3_histogram_tag_packet256_rx/ilc3_histogram_tag_packet256_rx.xpr
+reset_run synth_1
 reset_run impl_1
 launch_runs impl_1 -to_step write_bitstream -jobs 4
 wait_on_run impl_1
